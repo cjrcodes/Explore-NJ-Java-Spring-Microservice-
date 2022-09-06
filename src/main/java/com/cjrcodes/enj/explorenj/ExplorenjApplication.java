10bfb8a2e12d4e73ce87d5b80f1b2be4b041a1c6
Christian <cjrcodes@gmail.com>
2022-09-06 11:39:03 -0400

    Project init commit

---
+++ src/main/java/com/cjrcodes/enj/explorenj/ExplorenjApplication.java
@@ -0,0 +1,13 @@
+package com.cjrcodes.enj.explorenj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorenjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorenjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cjrcodes/enj/explorenj/ExplorenjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjrcodes.enj.explorenj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorenjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
